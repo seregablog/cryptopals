4245baf2240cd25c25269372912034b5f75506d3
seregablog <seregaprojects@gmail.com>
2024-03-15 20:24:45 +0300

    lint

--- cryptopals/Set1/Challenge1/S1CH1.py
+++ cryptopals/Set1/Challenge1/S1CH1.py
@@ -43,4 +43,5 @@ class Base64:
 if __name__ == "__main__":
     b = Base64()
     hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    print('Hex: ', hex)
     print('Base64:', b.encodeHex(hex))
